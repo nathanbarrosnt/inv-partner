e8e0fa7b9603e74f85bf904deaa275d3fc473af4
Nathan <nathanbarrosnt@gmail.com>
2024-01-17 12:38:29 -0300

    release: structure

---
+++ src/main/java/com/neidev/invp/InvpApplication.java
@@ -0,0 +1,13 @@
+package com.neidev.invp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neidev/invp/InvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidev.invp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
